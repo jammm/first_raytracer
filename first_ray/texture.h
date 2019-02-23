f3d1e6a62ee67ca91b2e043df7fbb76f0bd0a4ce
jam <jem456.vasishta@gmail.com>
2019-02-23 22:44:41 +0900

    Add checkerboard texture class

--- first_ray/texture.h
+++ first_ray/texture.h
@@ -2,6 +2,7 @@
 #define TEXTURE_H_
 
 #include "vec3.h"
+#include <cmath>
 
 class texture
 {
@@ -20,6 +21,24 @@ public:
     }
 
     vec3 color;
+};
+
+class checker_texture : public texture
+{
+public:
+    checker_texture() {}
+    checker_texture(texture *t0, texture *t1) : even(t0), odd(t1) {}
+    virtual vec3 value(const float &u, const float &v, const vec3 &p) const
+    {
+        float sines = sin(10*p.x()) * sin(10*p.y()) * sin(10*p.z());
+        if (sines < 0.0f)
+            return odd->value(u, v, p);
+        else
+            return even->value(u, v, p);
+    }
+
+    texture *odd;
+    texture *even;
 };
 
 #endif
