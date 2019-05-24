a9a8cd2b87e0698335d075e48f7850b67b9b8b0f
Aaryaman Vasishta <jem456.vasishta@gmail.com>
2019-05-24 18:53:00 +0900

    Add Orthonormal basis for surface normal

---
+++ first_ray/onb.h
@@ -0,0 +1,34 @@
+#ifndef ONB_H_
+#define ONB_H_
+
+class onb
+{
+public:
+    onb() {}
+    inline Vector3f operator[](int i) const { return axis[i]; }
+    Vector3f u() const { return axis[0]; }
+    Vector3f v() const { return axis[1]; }
+    Vector3f w() const { return axis[2]; }
+    Vector3f local(const float &a, const float &b, const float &c) { return a*u() + b*v() + c*w(); }
+    Vector3f local(const Vector3f &a) const { return a.x()*u() + a.y()*v() + a.z()*w(); }
+    void build_from_w(const Vector3f & n);
+
+
+
+    Vector3f axis[3];
+};
+
+void onb::build_from_w(const Vector3f &n)
+{
+    axis[2] = unit_vector(n);
+    Vector3f a;
+    if (fabs(w().x()) > 0.9)
+        a = Vector3f(0, 1, 0);
+    else
+        a = Vector3f(1, 0, 0);
+
+    axis[1] = unit_vector(cross(w(), a));
+    axis[0] = cross(w(), v());
+}
+
+#endif
