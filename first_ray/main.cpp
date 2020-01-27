dedac1ec228d81d4cb1fcde3cc5da745856a028f
jam <jem456.vasishta@gmail.com>
2020-01-28 02:14:25 +0900

    Update main.cpp to use path tracer integrator

--- first_ray/main.cpp
+++ first_ray/main.cpp
@@ -12,19 +12,23 @@
 #include "aarect.h"
 #include "box.h"
 #include "pdf.h"
-#include "viewer.h"
-#include "sampler.h"
-
-// Include renderers
-#include "integrator.h"
 #include "path.h"
 #include "path_prt.h"
 #include "ao.h"
 #include "debug_renderer.h"
-
-// Other includes
 #include <float.h>
+#include <taskflow/taskflow.hpp>
 #include <chrono>
+
+#if defined(_WIN32) || defined(__linux__)
+#define GLEW_STATIC
+#include <GL/glew.h>
+#define _CRT_SECURE_NO_DEPRECATE
+#endif
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#endif
+#include <GLFW/glfw3.h>
 
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
@@ -42,6 +46,11 @@ inline Vector3f de_nan(const Vector3f &c)
     return temp;
 }
 
+void glfw_error_callback(int, const char* err_str)
+{
+    std::cout << "GLFW Error: " << err_str << std::endl;
+}
+
 hitable *random_scene(camera &cam, const float &aspect, std::vector<hitable *> &lights)
 {
     /* n == number of spheres */
@@ -49,7 +58,6 @@ hitable *random_scene(camera &cam, const float &aspect, std::vector<hitable *> &
     hitable **list = new hitable*[n + 1];
     //Large sphere's texture can be checkered
     texture *checker = new checker_texture(new constant_texture(Vector3f(0.2f, 0.3f, 0.1f)), new constant_texture(Vector3f(0.99f, 0.99f, 0.99f)));
-    sampler s;
 
     list[0] = new sphere(Vector3f(0, -1000, 0), 1000, new lambertian(checker));
     int i = 1;
@@ -57,20 +65,20 @@ hitable *random_scene(camera &cam, const float &aspect, std::vector<hitable *> &
     {
         for (int b = -11; b < 11; b++)
         {
-            float choose_mat = s.get1d();
-            Vector3f center(a + 0.9f * s.get1d(), 0.2f, b + 0.9f * s.get1d());
+            float choose_mat = gen_cano_rand();
+            Vector3f center(a + 0.9f * gen_cano_rand(), 0.2f, b + 0.9f * gen_cano_rand());
             if ((center - Vector3f(4, 0.2f, 0)).length() > 0.9f)
             {
                 if (choose_mat < 0.8)
                 {
                     //diffuse
-                    list[i++] = new sphere(center, 0.2f, new lambertian(new constant_texture(Vector3f(s.get1d() * s.get1d(), s.get1d() * s.get1d(), s.get1d() * s.get1d()))));
+                    list[i++] = new sphere(center, 0.2f, new lambertian(new constant_texture(Vector3f(gen_cano_rand() * gen_cano_rand(), gen_cano_rand() * gen_cano_rand(), gen_cano_rand() * gen_cano_rand()))));
                 }
                 else if (choose_mat < 0.95)
                 {
                     //metal
                     list[i++] = new sphere(center, 0.2f,
-                        new metal(Vector3f(0.5f*(1 + s.get1d()), 0.5f*(1 + s.get1d()), 0.5f*(1 + s.get1d())), 0.5f*s.get1d()));
+                        new metal(Vector3f(0.5f*(1 + gen_cano_rand()), 0.5f*(1 + gen_cano_rand()), 0.5f*(1 + gen_cano_rand())), 0.5f*gen_cano_rand()));
                 }
                 else
                 {
@@ -288,14 +296,40 @@ hitable *veach_mis(camera &cam, const float &aspect, std::vector<hitable *> &lig
     //return parallel_bvh_node::create_bvh(list, i, 0.0f, 0.0f);
 }
 
+void background_thread(const std::shared_future<void> &future, GLubyte *out_image, GLFWwindow* window, int nx, int ny, bool &to_exit)
+{
+    while (!to_exit)
+    {
+        /* Poll for and process events */
+        glfwPollEvents();
+        /* Render here */
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, nx, ny, 0, GL_RGB, GL_UNSIGNED_BYTE, out_image);
+        glBlitFramebuffer(0, 0, nx, ny, 0, 0, nx, ny,
+            GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        if (future.wait_for(std::chrono::seconds(0)) == std::future_status::ready || glfwWindowShouldClose(window))
+        {
+            to_exit = true;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
 int main(int argc, const char **argv)
 {
     constexpr int nx = 1024;
     constexpr int ny = 768;
     int ns = 100;
     constexpr int comp = 3; //RGB
+    auto out_image = std::make_unique<GLubyte[]>(nx * ny * comp + 64);
+    auto fout_image = std::make_unique<GLfloat[]>(nx * ny * comp + 64);
+    memset(out_image.get(), 0, nx * ny * comp + 64);
+    memset(fout_image.get(), 0.0f, nx * ny * comp + 64);
     //out_image = (GLubyte *)(((std::size_t)out_image) >> 6 <<6);
-    viewer film_viewer(nx, ny, ns, comp);
+    bool to_exit = false;
 
     /* Parse command line args */
     if (argc == 3)
@@ -310,6 +344,9 @@ int main(int argc, const char **argv)
     std::cout<<"Resolution: "<<nx<<"x"<<ny<<std::endl;
     std::cout<<"Setting number of samples to "<<ns<<std::endl;
 
+    // Use cpp-taskflow https://github.com/cpp-taskflow/cpp-taskflow
+    tf::Taskflow tf;
+
     // Start performance timer
     std::chrono::high_resolution_clock::time_point t11 = std::chrono::high_resolution_clock::now();
 
@@ -321,13 +358,142 @@ int main(int argc, const char **argv)
     std::chrono::duration<double> time_spann = std::chrono::duration_cast<std::chrono::duration<double>>(t22 - t11);
     std::cout << "\nBVH construction took me " << time_spann.count() << " seconds.";
 
+    GLFWwindow* window;
+
+    /* Register GLFW error callback */
+    glfwSetErrorCallback(glfw_error_callback);
+    /* Initialize GLFW */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    /* Also specify the OpenGL version (seems like this is sensitive to many potential issues) */
+#if defined(__APPLE__) || defined(_WIN32)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+#else
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#endif
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GL_FALSE);
+#endif
+
+#ifdef __APPLE__
+    /* OSX requires forward compatibility for some reason */
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    window = glfwCreateWindow(nx, ny, "jam's ray tracer", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "ERROR: cannot open window with GLFW3\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+#if defined(_WIN32) || defined(__linux__)
+    /* Initialize GLEW */
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        // Problem: glewInit failed, something is seriously wrong.
+        std::cout << "glewInit failed: " << glewGetErrorString(err) << std::endl;
+        exit(1);
+    }
+#endif
+
+    /* Create texture used to represent the color buffer */
+    GLuint render_texture;
+    glGenTextures(1, &render_texture);
+    glBindTexture(GL_TEXTURE_2D, render_texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, nx, ny, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+    /* Create FBO to represent the framebuffer for blitting to the screen */
+    GLuint fbo;
+    glGenFramebuffers(1, &fbo);
+
+    /* Bind FBO to both GL_FRAMEBUFFER and GL_READ_FRAMEBUFFER */
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, render_texture, 0);
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+
+    /* Clear the window */
+    glClear(GL_COLOR_BUFFER_BIT);
+
     // Use the renderer specified in template parameter
-    renderer<path> render;
+    path renderer;
 
-    render.Render(scene.get(), film_viewer);
+    tf.parallel_for(ny - 1, 0, -1, [&](int j)
+    {
+        for (int i = 0; i < nx; i++)
+        {
+            //if (i <= 512)
+            {
+                if (to_exit) break;
+                Vector3f col(0.0f, 0.0f, 0.0f);
+                for (int s = 0; s < ns; s++)
+                {
+                    float u = float(i + gen_cano_rand()) / float(nx);
+                    float v = float(j + gen_cano_rand()) / float(ny);
+                    ray r = scene->cam.get_ray(u, v);
+                    hit_record hrec;
+                    const Vector3f sample = renderer.Li(r, scene.get(), 0, hrec, 0.0f);
+                    assert(std::isfinite(sample[0])
+                           && std::isfinite(sample[1])
+                           && std::isfinite(sample[2]));
+                    col += sample;
+                }
+                col /= float(ns);
 
-    // Save film to file(s) (currently JPG,PNG and PFM)
-    film_viewer.save_and_destroy();
+                float fr = col[0];
+                float fg = col[1];
+                float fb = col[2];
+
+                int ir = std::min(int(pow(fr, 1.0f/2.2f) * 255.9999), 255);
+                int ig = std::min(int(pow(fg, 1.0f/2.2f) * 255.9999), 255);
+                int ib = std::min(int(pow(fb, 1.0f/2.2f) * 255.9999), 255);
+                int index = (j * nx + i) * comp;
+
+                // Store output pixels
+                out_image[index] = (GLubyte)ir;
+                out_image[index + 1] = (GLubyte)ig;
+                out_image[index + 2] = (GLubyte)ib;
+
+                fout_image[index] = fr;
+                fout_image[index + 1] = fg;
+                fout_image[index + 2] = fb;
+            }
+        }
+        std::cout << ".";
+    });
+
+    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+
+    auto future = tf.dispatch();
+
+    // Refresh window in background
+    background_thread(future, out_image.get(), window, nx, ny, to_exit);
+
+    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+
+    std::cout << "\nIt took me " << time_span.count() << " seconds to render." << std::endl;
+
+    std::cout << "Saving BMP..." << std::endl;
+    image(out_image.get(), nx, ny, comp).save_image(formats::STBI_BMP);
+    std::cout << "Saving JPG..." << std::endl;
+    image(out_image.get(), nx, ny, comp).save_image(formats::STBI_JPG);
+    std::cout << "Saving PNG..." << std::endl;
+    image(out_image.get(), nx, ny, comp).save_image(formats::STBI_PNG);
+    std::cout << "Saving PFM..." << std::endl;
+    image_pfm(fout_image.get(), nx, ny, comp).save_image("out_test.pfm");
+
+    glfwTerminate();
 
     return 0;
 }
